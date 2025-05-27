6cd16d8be069508440bbb4b4790513b23927f95c
Harald Wilbertz <harald.wilbertz@gmx.de>
2025-05-27 21:25:13 +0200

    Additional requirement.

--- streamlit.py
+++ streamlit.py
@@ -1,18 +1,8 @@
 import streamlit as st
-from index import build_index
-from documents import get_documents
-from settings import initialize_settings
 
 def main():
     st.title("Everything you wanted to know about KPMG Denmark but were afraid to ask:")
-    st.write(st.secrets["AZURE_OPEN_API_KEY"])
-
-    initialize_settings()
-
-    documents = get_documents()
-    index = build_index(documents)
-    chat_engine = index.as_chat_engine()
-
+    #st.write(st.secrets["AZURE_OPEN_API_KEY"])
 
 
 if __name__ == "__main__":
